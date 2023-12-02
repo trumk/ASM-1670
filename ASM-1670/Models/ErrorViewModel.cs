457fe281d318395e466b3e183adf458a43df968c
trumk <nguyentrungtest2292003@gmail.com>
2023-12-02 12:02:44 +0700

    Add project files.

---
+++ ASM-1670/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASM_1670.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
