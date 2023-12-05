5bc54e51b62aa36926dc8030894b347d145e1963
trumk <nguyentrungtest2292003@gmail.com>
2023-12-05 08:33:16 +0700

    connect AspNetUser

--- ASM-1670/Controllers/AdminController.cs
+++ ASM-1670/Controllers/AdminController.cs
@@ -15,16 +15,10 @@ namespace ASM_1670.Controllers
         // Example action to update user phone number
 
         // Action to view user information
-        public async Task<IActionResult> User(string userId)
+        public IActionResult User()
         {
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
-            {
-                // Handle the case where the user is not found
-                return NotFound();
-            }
-
-            return View(user);
+            var users = _userManager.Users.ToList();
+            return View(users);
         }
 
     }
