605c8d56aa1e1b0ae6ca0dfeb90a465e564e65ca
trumk <nguyentrungtest2292003@gmail.com>
2023-12-05 11:22:25 +0700

    admin dashboard demo

--- ASM-1670/Controllers/AdminController.cs
+++ ASM-1670/Controllers/AdminController.cs
@@ -1,24 +1,36 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ASM_1670.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASM_1670.Controllers
 {
     public class AdminController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _context;
 
-        public AdminController(UserManager<IdentityUser> userManager)
+        public AdminController(UserManager<IdentityUser> userManager, ApplicationDbContext db)
         {
             _userManager = userManager;
+            _context = db;
         }
 
         // Example action to update user phone number
 
-        // Action to view user information
+        public IActionResult Index()
+        {
+            return View();
+        }
         public IActionResult User()
         {
             var users = _userManager.Users.ToList();
             return View(users);
+        }
+        public IActionResult Order()
+        {
+            var orders = _context.Orders.ToList();
+            return View(orders);
         }
 
     }
